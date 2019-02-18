8f75e665d6f225943d92db3dc6cb15ad5a16edcc
karldivad <carlosvalenciarivero@gmail.com>
2019-02-18 15:10:43 -0500

    Introduce game ending by winner calculation

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
     handleClick(i){
         // not mutating approach:
         const squares = this.state.squares.slice(); // to create a copy
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares:squares,
@@ -38,7 +41,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status = 'Winner: ' + winner;
+        }else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -77,6 +86,28 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+export function calculateWinner(squares) {
+    const winnerPositions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winnerPositions.length; i++){
+        const [p0, p1, p2] = winnerPositions[i];
+        if (squares[p0] && squares[p0] === squares[p1] && squares[p0] === squares[p2]){
+            return squares[p0];
+        }
+    }
+    return null;
+
 }
 
 // ========================================
